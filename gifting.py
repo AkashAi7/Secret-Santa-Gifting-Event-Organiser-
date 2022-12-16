bfe40134904d2c1c3b993c66ee8d2f5c5a1b0d1b
Akash Dwivedi <46550108+AkashAi7@users.noreply.github.com>
2022-12-16 18:34:08 +0530

    updated the column names for the form fields

--- gifting.py
+++ gifting.py
@@ -19,12 +19,12 @@ mail = outlook.CreateItem(0)
 import random
 import pandas as pd 
 import copy
-df = pd.read_csv('secretsanta.csv') 
+df = pd.read_csv('secretsanta.csv') #Enter your .csv form file name here 
 mylist = df['Name'].tolist()
 names=mylist
-emails=(df['Name']+" "+df['Email ']).tolist()
-num=df['Phone number'].tolist()
-email2=df['Email '].tolist()
+emails=(df['Name']+" "+df['Email']).tolist()
+num=df['Phone_Number'].tolist()
+email2=df['Email'].tolist()
 
 def secret_santa(names):
     my_list = names
@@ -47,7 +47,7 @@ ss_result = [list(ele) for ele in ss_result]
 final = zip(ss_result,emails,num,email2)
 
 
-df2 = pd.DataFrame(final, columns = ['Name', 'Name2','Phone number','email2'])
+df2 = pd.DataFrame(final, columns = ['Name', 'Name2','Phone_Number','email2'])
 
 df2[['Reciver','Sender']] = pd.DataFrame(df2.Name.tolist(), index= df2.index)
 
@@ -59,36 +59,35 @@ df2['Name']=df2['Name'].astype(str)
 
 
 
-df3=pd.merge(df2,df,on="Phone number")
+df3=pd.merge(df2,df,on="Phone_Number")
 
 
 
 df3=df3.drop(['Name_x', 'Name2','Name_y'], axis=1)
+
 df3.rename(columns = {'Sender':'Name'}, inplace = True)
 
 
 
 df4=pd.merge(df3,df,on="Name")
 
-# df4.dtypes
 
-df4=df4.drop(['Email _x','Address_y','Phone number_y',
-              'Wishlist Gift-1 _y','Wishlist Gift-2_y','Wishlist Gift-3_y'],axis=1)
+#df.dtypes
+#df2.dtypes
+#df3.dtypes
+df4.dtypes
 
-df4.rename(columns = {'Phone number_x':'Reciver_Phone_Number' ,
+df4=df4.drop(['Email_x','Address_y','Phone_Number_y',
+              'Wishlist_Gift_1_y','Wishlist_Gift_2_y','Wishlist_Gift_3_y'],axis=1)
+
+df4.rename(columns = {'Phone_Number_x':'Reciver_Phone_Number' ,
                       'email2':'Reciver_email',
-                      'Name':'Sender_Name','Email _y':'Sender_email'
-                      ,'Address_x':'Reciver Address'}, inplace = True)
-
-
-
-
-
-
+                      'Name':'Sender_Name','Email_y':'Sender_email'
+                      ,'Address_x':'Reciver_Address'}, inplace = True)
 
 df4.to_excel('Gifting_Final.xlsx',index=0)
 
-for sendername,sender_email,reciver_name,recadd,recnum,item1,item2,item3 in zip(df4['Sender_Name'], df4['Sender_email'], df4['Reciver'],df4['Reciver Address'],df4['Reciver_Phone_Number'],df4['Wishlist Gift-1 _x'],df4['Wishlist Gift-2_x'],df4['Wishlist Gift-3_x']):
+for sendername,sender_email,reciver_name,recadd,recnum,item1,item2,item3 in zip(df4['Sender_Name'], df4['Sender_email'], df4['Reciver'],df4['Reciver_Address'],df4['Reciver_Phone_Number'],df4['Wishlist_Gift_1_x'],df4['Wishlist_Gift_2_x'],df4['Wishlist_Gift_3_x']):
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = sender_email
@@ -104,7 +103,7 @@ for sendername,sender_email,reciver_name,recadd,recnum,item1,item2,item3 in zip(
     <b>Following is the gifting details of  """ +reciver_name+"""</b ><rr>
      "<ul>
      <li> <b>Address</b> 🏠:- """ + recadd + """  <br>
-     <li> <b> Phone Number ☎️ </b>:- """+ str(recnum)+"""  <br>
+     <li> <b> Phone_Number ☎️ </b>:- """+ str(recnum)+"""  <br>
       
       </ul>
       <b> Wish list Items 🌠 </b><br> 
